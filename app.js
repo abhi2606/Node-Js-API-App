d34761ead1d639271668b02add0476c44efc98d4
Abhishek Sahu <abhishek@Abhisheks-MacBook-Air.local>
2024-10-13 14:46:07 +0530

    Inital Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+
+// API endpoint URL
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+// Make API request using axios
+axios.get(apiUrl)
+  .then(response => {
+    const data = response.data;
+
+    // Render EJS template with API data
+    app.get('/', (req, res) => {
+      res.render('index', { data });
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});     
